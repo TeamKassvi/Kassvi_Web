e009017a10c5ea12ebbdaa563684332dafc4b65d
Suvigya <suvi7896@gmail.com>
2017-11-28 18:38:05 +0530

    Shifting from Awaaz to Kassvi

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Awaaz");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Kassvi");
 
 module.exports = {mongoose};
