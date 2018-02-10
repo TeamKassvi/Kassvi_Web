172c4bc26f1a3aaee4e1d836b140df84159c39c9
Parikshit Hooda <phooda804@live.com>
2018-02-10 20:29:26 +0530

    ffmpeg-win test successful.working on integration

--- controllers/emotionController.js
+++ controllers/emotionController.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const Emotion = mongoose.model('Emotion');
 const multer = require('multer');
-const request = require('request');
+// const request = require('request');
 const path = require('path');
 const ffmpeg = require('ffmpeg-win');
-const Extractor = require("html-extractor");
-const rp = require('request-promise').defaults({simple:false});
+// const Extractor = require("html-extractor");
+// const rp = require('request-promise').defaults({simple:false});
 var fs = require('fs');
+const Ffmpeg = require('./test');
 
 
 
@@ -46,20 +47,21 @@ if(!req.file)
     return;
   }
   console.log(req.file);
+  Ffmpeg.wavConvert();
 //correct above
 
-function done(err) {
-  if (err) throw err;
-  console.log('ok, done');
-}
-
-const tempName = req.file.filename.split('.');
-const wavName = tempName[0];
-console.log(wavName);
+// function done(err) {
+//   if (err) throw err;
+//   console.log('ok, done');
+// }
+//
+// const tempName = req.file.filename.split('.');
+// const wavName = tempName[0];
+// console.log(wavName);
 
 // console.log(wavName[0]);
 
-ffmpeg.wav(req.file.path, `/public/audioUploads/${wavName}.wav`, done);
+// ffmpeg.wav(`./${req.file.path}`, `./audioUploads/${wavName}.wav`, done);
 
 
 };
--- test.js
+++
@@ -1,7 +0,0 @@
-const ffmpeg = require('ffmpeg-win');
-function done(err) {
-  if (err) throw err;
-  console.log('ok, done');
-}
-
-ffmpeg.wav('./public/audioUploads/uploadFile-1518141022079.mp3', './x.wav', done);
